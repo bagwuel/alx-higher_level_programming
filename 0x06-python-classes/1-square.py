5c5547199e032867f3d5c621775b994d0d787dab
bagwuel <mbagwu39@gmail.com>
2023-06-28 01:06:12 +0000

    complete task 1 and correct task 0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a Square object.
+
+        Args:
+            size: The size of the Square object.
+        """
+        self.__size = size
